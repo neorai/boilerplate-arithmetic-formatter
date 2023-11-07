82eb29564aa0afc697a178a850baa4cc133406ca
Robert <32270269+neorai@users.noreply.github.com>
2023-11-07 18:45:54 +0100

    implement rjust

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -22,7 +22,7 @@ def check_sintax_errors(problems_list, dic_problems):
         
 def arithmetic_arranger(problems_list, display_result = False):
     dic_problems = list_to_dic(problems_list)
-    error        = check_sintax_errors(problems_list, dic_problems)
+    error = check_sintax_errors(problems_list, dic_problems)
     if error: return error
     
     arranged_problems = ""
@@ -30,48 +30,36 @@ def arithmetic_arranger(problems_list, display_result = False):
     second_row = ""
     third_row  = ""
     fourth_row = "" 
-    counter = 0
     
     for key, value in dic_problems.items():
-        len_value0 = len(value[0])
-        len_value2 = len(value[2])
+        int_value0 = int(value[0])
+        int_value2 = int(value[2])   
         len_dic_problems = len(dic_problems)
-        
-        bigger_len_value = len_value0 if len_value0 >= len_value2 else len_value2
-        dashes       = "-" * (abs(bigger_len_value) + 2)
-        break_line   = "\n" if counter == (len_dic_problems -1) else "    "
-        break_result = ""   if counter == (len_dic_problems -1) else "    "
-        break_dash   = ""   if counter == (len_dic_problems -1) else "    "
-        
-        if display_result:
-            if value[1] == "+":
-                result = int(value[0]) + int(value[2])
-            if value[1] == "-":
-                result = int(value[0]) - int(value[2])
-                
-            len_spc        = (abs(bigger_len_value) + 2) - len(str(result))
-            spc_fourth_row = " " * len_spc
-            fourth_row     += spc_fourth_row + str(result) + break_result
-            break_dash     = "\n" if counter ==  (len_dic_problems -1)  else "    "
-                       
-        if int(value[0]) >= int(value[2]):
-            spc_first_row  = 2
-            spc_second_row = abs(len_value0 - len_value2)
-        else:    
-            spc_first_row  = abs(len_value0 - len_value2) + 2
-            spc_second_row = 0
+           
+        if value[1] == "+":
+            result = int_value0 + int_value2
+        else:
+            result = int_value0 - int_value2
+                    
+        num_list = [int(len(value[0])),int(len(value[2]))]
+        num_max = max(num_list) + 2
+        dashes  = "-" * num_max 
 
-        r_aling_first_row  = " " * spc_first_row
-        r_aling_second_row = " " * spc_second_row
-
-        first_row  += r_aling_first_row + value[0] + break_line
-        second_row += value[1]          + " "      + r_aling_second_row + value[2] + break_line
-        third_row  += dashes            + break_dash
-       
-        counter += 1
-
-    arranged_problems = first_row + second_row + third_row + fourth_row
+        if key == (len_dic_problems -1):
+            white_space = ""
+        else:
+            white_space = "    "
+            
+        first_row  += str(value[0]).rjust(num_max) + white_space
+        second_row += value[1]                     + str(value[2]).rjust(num_max - 1) + white_space
+        third_row  += dashes                       + white_space
+        fourth_row += str(result).rjust(num_max)   + white_space  
         
+    if display_result:
+        arranged_problems = first_row + "\n" + second_row + "\n" + third_row + "\n" + fourth_row
+    else:
+        arranged_problems = first_row + "\n" + second_row + "\n" + third_row 
+           
     return arranged_problems
 
-print(arithmetic_arranger(['3401 - 2', '123 + 49']))
+print(arithmetic_arranger(['3401 - 2', '123 + 49'],True))
